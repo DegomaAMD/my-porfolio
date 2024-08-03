025f5fb1d652f55fe1950935f5ddc57a8a91a200
degomaamd <degoma.andria@gmail.com>
2024-08-03 19:24:07 +0800

    Added button for resume viewing and Modified CSS fot button hover effect

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import aboutImage from '../assets/img/3.webp'
+import resume from '../assets/document/AndriaMaeDegoma-Resume.pdf'
+import { Button } from '@mui/material';
 
 
 function About() {
+
+  const openDocument = () => {
+    window.open(resume, '_blank');
+  }
+
+
   return (
     <>
         <div className='aboutpage'>
@@ -14,6 +22,7 @@ function About() {
             </h1>
             <p>I have a strong passion for full-stack web development, with a keen interest in expanding my expertise in React.js and Node.js. My journey in web development began in 2022 after joining the kodeGo Full-stack web development Bootcamp, which kick-started my career. Since then, I've been continuously learning and taking on new projects. I'm dedicated to creating user-friendly and responsive website, ensuring they are not only good-looking but also perform well. Currently, I'm working on web applications using technologies like Wordpress, Flatsome page builder, HTML, CSS3, Javascript, ReactJS, NodeJS, and MySql.</p>
             <p>When I'm not coding, I'm probably checking out Facebook, hanging out with my kids and family, or just relaxing. If you need something from me, drop a message on the contact page, and I'll get back to you super fast.</p>
+            <Button className='viewResumeBtn' onClick={openDocument}><i class="fa-solid fa-file-arrow-down"></i> View my resume</Button>
           </div>
           
         </div>
